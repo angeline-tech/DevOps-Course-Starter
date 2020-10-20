5236901dc97363e2b8fef668086936b48d442caa
Jordan <jordan.angeline1998@gmail.com>
2020-10-20 17:37:04 +0100

    #1 can render index html and display the items from the session in a table

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
-
+from flask import Flask,render_template
+from todo_app.data.session_items import get_item,get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
+@app.route('/heartbeat')
+def heartbeat():
+    return 'Flask Server is Running'
+
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html',items=get_items())
 
 
 if __name__ == '__main__':
